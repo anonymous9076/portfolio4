e0c2ab4300fd5fdb324fadd41333f7edb916d486
anonymous9076 <vijaytushar915@gmail.com>
2024-10-20 14:12:05 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,187 @@
+const logotext = "Tushar";
+const meta = {
+    title: "Tushar Kumar",
+    description: "I’m Tushar Kumar Web Develper_ Full stack devloper,currently working in BoffinBlocks",
+};
+
+const introdata = {
+    title: "I’m Tushar Kumar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop Highly Interactive WebPages",
+    },
+    description: "Crafting digital experiences, one line of code at a time. Empowering your online presence with innovation and precision.",
+    your_img_url: "./images/myimg.jpeg",
+};
+
+const dataabout = {
+    title: "Abit about my self",
+    aboutme: `Myself Tushar kumar, I'm 22years old and I'm B.C.A graduate from GNDU university Affilated college ,SL bawa dav college, Batala. I am skilled web developer and a freelancer I build many project by my own to develop my skills and experience.`, 
+    aboutme2:"Outcome-focused web developer with good experience seeking a challenging position to leverage my expertise in front-end and back-end development. Committed to staying up-to-date with emerging web technologies and dedicated to delivering high- quality, responsive, and scalable web solutions that meet client needs"
+};
+const worktimeline = [{
+        jobtitle: "Product Designer",
+        where: "Gsons India",
+        date: "1Mar - 30May 2024",
+    },
+    {
+        jobtitle: "Full Stack Developer",
+        where: "bds Logistics",
+        date: "7Jul - 31Aug 2024",
+    },
+    {
+        jobtitle: "Jr Web Developer",
+        where: "ItSmith Software solution",
+        date: "9Sep - Still Serving",
+    },
+   
+];
+
+const skills = [{
+        name: "HTML5",
+        value: 99,
+    },
+    {
+        name: "CSS/TAILWIND CSS/BOOTSTRAP CSS",
+        value: 99,
+    },
+   
+    {
+        name: "Javascript",
+        value: 99,
+    },
+    {
+        name: "React JS",
+        value: 90,
+    },
+    {
+        name: "NODE JS",
+        value: 75,
+    },
+    {
+        name: "EXPRESS JS",
+        value: 60,
+    },
+{
+        name: "MONGO DB",
+        value: 70,
+    },
+    {
+        name: "MYSQL",
+        value: 65,
+    },
+    {
+        name: "ANGULAR TS",
+        value: 60,
+    },
+    {
+        name: "TYPESCRIPT",
+        value: 75,
+    },
+    {
+        name: "C/C++",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "FRONTEND WEBSITES",
+        description: "Developed dynamic and responsive websites using modern front-end technologies, ensuring a seamless user experience across different devices. Successfully created user-friendly interfaces for projects like Diginotes and GSONSIndia, focusing on clear navigation and visually appealing layouts. Implemented best practices in HTML, CSS, and JavaScript to enhance accessibility, improve load times, and deliver a polished look and feel."   },
+    {
+        title: "BACKEND SCRIPTs & APIs",
+        description: "Developed robust back-end scripts to support the functionality of e-commerce stores, integrating secure payment gateways and user authentication. Designed and implemented efficient server-side solutions for projects like Diginotes, ensuring data management and access control. Focused on optimizing back-end processes to improve performance, security, and scalability across web applications.",
+    },
+    {
+        title: "PRODUCT DESIGNS",
+        description: "Engaged in designing digital product interfaces that prioritize usability and enhance the overall user journey. Created visually appealing layouts for web-based projects, such as the e-commerce store and Diginotes, ensuring consistency and brand alignment. Applied principles of user-centered design to deliver products that effectively meet client requirements and user expectations.",
+    },
+     {
+        title: "SCRIPTING",
+        description: "Developed custom scripts for automating tasks and processes within web development projects, enhancing workflow efficiency. Used scripting languages to manage data handling and back-end processes, especially in projects like the e-commerce platform. Leveraged scripting skills to streamline tasks, from database management to server-side logic, ensuring seamless integration with front-end components.",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "./images/brave_screenshot_dgnotes.vercel.app.png ",
+        description: " ",
+        link: "https://dgnotes.vercel.app",
+    },
+    {
+        img: " ./images/brave_screenshot_www.gsonsindia.in.png",
+        description: " ",
+        link: "https://gsonsindia.in ",
+    },
+    {
+        img: "./images/brave_screenshot_69airline.vercel.app.png",
+        description: " ",
+        link: " https://69airline.vercel.app",
+    },
+    {
+        img: "./images/brave_screenshot_bdslogistics.vercel.app.png ",
+        description: " ",
+        link: "https://bdslogistics.vercel.app ",
+    },
+  
+    {
+        img: " ./images/brave_screenshot_ellocent.vercel.app.png",
+        description: " ",
+        link: " https://ellocent.vercel.app",
+    },
+    {
+        img: "./images/brave_screenshot_freezy.vercel.app.png ",
+        description: " ",
+        link: "https://freezy.vercel.app ",
+    },
+    {
+        img: "./images/brave_screenshot_headsetmart.vercel.app.png ",
+        description: " ",
+        link: " https://headsetmart.vercel.app",
+    },
+    {
+        img: "./images/brave_screenshot_osm-mosam.netlify.app.png ",
+        description: " ",
+        link: "https://osm-mosam.netlify.app",
+    },
+    {
+        img: "./images/brave_screenshot_piratestore.netlify.app.png ",
+        description: " ",
+        link: " https://piratestore.netlify.app",
+    },
+    {
+        img: " ./images/brave_screenshot_therealplace.netlify.app.png",
+        description: " ",
+        link: " https://therealplace.netlify.app",
+    },
+ 
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "tushar32234@gmail.com",
+    YOUR_FONE: "+91 8264829755",
+    description: "Crafting digital experiences, one line of code at a time. Empowering your online presence with innovation and precision.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/anonymous9076",
+    instagram: "https://instagram.com/__tushar__4369/",
+    linkedin: "https://linkedin.com/in/tushar-kumar-903636280",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
